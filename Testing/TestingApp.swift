c4751e91c8a31f0040910a37f6e5f8aa89fdd041
Romanos Hiliarhopoulos <romanoshiliarhopoulos@gmail.com>
2024-03-09 13:27:26 +0200

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Romanos Hiliarhopoulos on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
